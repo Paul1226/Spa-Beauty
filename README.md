0da1da8526cc7923bd93ab61d3f143fee94bab7e
Paul Guachichullca <paulguachichullca023@gmail.com>
2024-05-01 22:17:52 -0500

    Proyecto inicializado en blanco con fuentes y colores configurados

---
+++ README.md
@@ -0,0 +1,4 @@
+# Spa Beauty
+## Se irá añadiendo las modificaciones que van quedando pendientes dentro del repositorio
+
+- [_] 
---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily:{
+				'poppins':['Poppins', 'sans-serif'],
+			},
+			colors:{
+				primary: '#264065',
+				secondary: '#A9D6CB',
+				tertiary: '#909090',
+				quaternary: '#070D15'
+
+			}
+		},
+	},
+	plugins: [],
+}
